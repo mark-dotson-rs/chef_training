47ee76108831904bf1230aa6106ae9fc12f8f540
mark-dotson-rs <mark.dotson@rightscale.com>
2012-01-25 22:48:20 +0000

    work this time

--- cookbooks/enhanced_hello_world/metadata.rb
+++ cookbooks/enhanced_hello_world/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures enhanced_hello_world"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe "enhanced_hello_world::default", "This hello world is da bomb"
