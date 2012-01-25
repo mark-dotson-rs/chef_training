0f1e014228cf042beeb66e14d9ceca87cf1f02a5
mark-dotson-rs <mark.dotson@rightscale.com>
2012-01-25 22:44:47 +0000

    once again with flair

---
+++ cookbooks/enhanced_hello_world/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "RightScale"
+maintainer_email "mark.dotson@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures enhanced_hello_world"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ cookbooks/enhanced_hello_world/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: enhanced_hello_world
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello world damnit!"
